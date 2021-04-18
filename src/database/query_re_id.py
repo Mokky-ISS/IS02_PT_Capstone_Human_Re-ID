3cec2f94ccaf38b141457534dd19c4f4263c527e
mokky05 <48450045+mokky05@users.noreply.github.com>
2021-04-18 23:54:05 +0800

    Update demo dashboard

--- src/database/query_re_id.py
+++ src/database/query_re_id.py
@@ -6,5 +6,31 @@ class DbQuery(DbQuery):
         super().__init__('database/reid_db.db')
 
     def get_human_id_options(self):
-        df_ids = super().query_data('SELECT DISTINCT(human_id) FROM human_table')
-        return [{'label': id, 'value': id} for id in df_ids['human_id'].values]
+        query = "SELECT DISTINCT(human_id) FROM human_table"
+        df_ids = super().query_data(query)
+        return [{'label': id, 'value': id} for id in df_ids['human_id'].values if int(id) > 0]
+
+    def get_images(self, human_id=None, start_datetime=None, end_datetime=None):
+        query = "SELECT h.img_id, h.human_id, h.inference_datetime, v.img FROM human_table AS h"
+        query += " INNER JOIN vectorkb_table AS v ON h.img_id=v.img_id"
+        if human_id is not None:
+            if " WHERE " in query:
+                query += " AND"
+            else:
+                query += " WHERE"
+            query += f" h.human_id == '{human_id}'"
+        if start_datetime is not None:
+            if " WHERE " in query:
+                query += " AND"
+            else:
+                query += " WHERE"
+            query += f" h.inference_datetime >= '{start_datetime}'"
+        if end_datetime is not None:
+            if " WHERE " in query:
+                query += " AND"
+            else:
+                query += " WHERE"
+            query += f" h.inference_datetime <= '{end_datetime}'"
+        query += " ORDER BY h.inference_datetime"
+        #print(query)
+        return super().query_data(query)
--- src/demo_dash/dash_app_v2.py
+++ src/demo_dash/dash_app_v2.py
@@ -5,7 +5,7 @@ from dash_bootstrap_components._components.Row import Row
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from src.demo_dash import header_v2 as header
 from src.database import query_image, query_re_id
 from urllib.parse import parse_qs
@@ -69,6 +69,7 @@ def display_page(pathname, search):
     return layout_page  # , title
 
 
+
 SIDEBAR_STYLE = {
     "position": "static",
     "top": 0,
@@ -94,19 +95,14 @@ def view1_page_content():
         children=[
             html.P('Human ID:'),
             dbc.Spinner(dcc.Dropdown(
-                id='human-id',
+                id='view1-human-id',
                 options=dbreid.get_human_id_options(),
             )),
             html.Br(),
             dbc.Row([
-                dbc.Col(html.P('From:'), width=2),
-                dbc.Col(dcc.DatePickerSingle(
-                    children='Start Time:', id='view1-starttime'), width='auto')]),
-            html.Br(),
-            dbc.Row([
-                dbc.Col(html.P('To:'), width=2),
-                dbc.Col(dcc.DatePickerSingle(
-                    children='End Time:', id='view1-endtime'), width='auto')]),
+                dbc.Col(html.P('Time:'), width=2),
+                dbc.Col(dcc.DatePickerRange(id='view1-datetime'), width='auto'),
+                ]),
             html.Br(),
             html.P('Camera/location:'),
             dbc.Spinner(dcc.Dropdown(id='view1-cam-loc-dropdown', options=CAMERA_LOCATION_OPTIONS)),
@@ -122,16 +118,16 @@ def view1_page_content():
             children=[
                 html.H5('Images:'),
                 dbc.Spinner(dbc.Row(
-                    id='images_row',
+                    id='view1-images-row',
                     form=True,
                     style={'flex-wrap': 'nowrap', 'overflow':'auto'})),
                 html.Br(),
-                html.H5('Images filename:'),
+                html.H5('Logs:'),
                 dbc.Spinner(dbc.Row(
                     id='logs_row',
                     children=
                         dcc.Textarea(
-                            id='logs_text',
+                            id='view1-logs-text',
                             disabled=True,
                             style={'width': '100%','height': '200px'}
                             ),
@@ -147,11 +143,17 @@ def view2_page_content():
         id='view2-page-sidebar',
         children=[
             html.P('Camera/location:'),
-            dbc.Spinner(dcc.Dropdown(id='view1-cam-loc-dropdown',
+            dbc.Spinner(dcc.Dropdown(id='view2-cam-loc-dropdown',
                                      options=CAMERA_LOCATION_OPTIONS)),
             html.Br(),
-            html.P('Timestamp:'),
-            dbc.Spinner(dcc.DatePickerSingle(id='view2-timestamp')),
+            dbc.Row([
+                dbc.Col(html.P('Time:'), width=2),
+                dbc.Col(dcc.DatePickerRange(
+                    id='view2-datetime'), width='auto'),
+            ]),
+            html.Br(),
+            dbc.Button(children='Refresh', id='view2-btn-refresh',
+                       color='primary', block=True, size="lg"),
         ],
         width=2,
         style=SIDEBAR_STYLE,
@@ -164,15 +166,15 @@ def view2_page_content():
             children=[
                 html.H5('Images:'),
                 dbc.Spinner(dbc.Row(
-                    id='images_row',
+                    id='view2-images-row',
                     form=True,
                     style={'flex-wrap': 'nowrap', 'overflow': 'auto'})),
                 html.Br(),
-                html.H5('Images filename:'),
+                html.H5('Logs:'),
                 dbc.Spinner(dbc.Row(
-                    id='logs_row',
+                    id='view2-logs-row',
                     children=dcc.Textarea(
-                            id='logs_text',
+                            id='view2-logs-text',
                             disabled=True,
                             style={'width': '100%', 'height': '200px'}
                             ),
@@ -182,163 +184,58 @@ def view2_page_content():
         )
     ])
 
-@app.callback(
-    Output(component_id='names-dropdown', component_property='options'),
-    Input(component_id='images-directory', component_property='value'),)
-def update_names(images_directory):
-    options = []
-    try:
-        for root, directories, _ in os.walk(images_directory, topdown=False):
-            for name in directories:
-                options.append({'label': name, 'value': os.path.join(root, name)})
-        return options
-    except Exception as ex:
-        logging.error(f'update_names: {ex}')
-        return []
-
 
 @app.callback(
-    Output(component_id='images_row', component_property='children'),
-    Output(component_id='logs_text', component_property='value'),
-    Input(component_id='names-dropdown', component_property='value'),
-    Input(component_id='detect-dropdown', component_property='value'),
+    Output(component_id='view1-images-row', component_property='children'),
+    Output(component_id='view1-logs-text', component_property='value'),
+    Input(component_id='view1-human-id', component_property='value'),
+    Input(component_id='view1-datetime', component_property='start_date'),
+    Input(component_id='view1-datetime', component_property='end_date'),
+    Input(component_id='view1-cam-loc-dropdown', component_property='value'),
 )
-def update_images(image_path, detect):
-    if image_path is not None:
-        images_col = []
-        logs_text = ''
-        try:
-            for root, _, files in os.walk(image_path, topdown=False):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    try:
-                        img = cv2.imread(file_path)
-                        if detect == 'face':
-                            img = detect_face(img)
-                        elif detect == 'face_mesh':
-                            img = detect_face_mesh(img)
-                        elif detect == 'hands':
-                            img = detect_hands(img)
-                        elif detect == 'pose':
-                            img = detect_pose(img)
-                        elif detect == 'holistic':
-                            img = detect_holistic(img)
-                        _, img_buffer = cv2.imencode('.png', img)
-                        encoded_image = base64.b64encode(img_buffer)# open(file_path, 'rb').read())
-                        images_col.append(
-                            dbc.Col(html.Img(
-                                src='data:image/png;base64,{}'.format(encoded_image.decode()),
-                                title=file,
-                                )))
-                        if len(logs_text) > 0:
-                            logs_text += '\n'
-                        logs_text += file
-                    except:
-                        pass
-            return images_col, logs_text
-        except Exception as ex:
-            logging.error(f'update_images: {ex}')
-    return [],[]
+def update_view1_images(human_id, start_date, end_date, cam_loc):
+    #print(human_id, start_date, end_date, cam_loc)
+    df_images = dbreid.get_images(human_id=human_id, start_datetime=start_date, end_datetime=end_date)
+    #print(df_images.columns)
+    images_col = []
+    for i in range(len(df_images)):
+        encoded_image = base64.b64encode(df_images['img'].iloc[i])
+        images_col.append(
+            dbc.Col(html.Img(
+                src='data:image/png;base64,{}'.format(
+                    encoded_image.decode()),
+                title=df_images['img_id'].iloc[i],
+            )))
+    #print(df_images[['img_id', 'human_id', 'inference_datetime']])
+    logs_text = df_images[['img_id', 'human_id',
+                           'inference_datetime']].to_string(justify='center')
+    return images_col, logs_text
 
-def detect_face(img):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_face_detection = mp.solutions.face_detection
-    with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
-        annotated_image = img.copy()
 
-        try:
-            # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
-            results = face_detection.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+@app.callback(
+    Output(component_id='view2-images-row', component_property='children'),
+    Output(component_id='view2-logs-text', component_property='value'),
+    Input(component_id='view2-btn-refresh', component_property='n_clicks'),
+    State(component_id='view2-cam-loc-dropdown', component_property='value'),
+    State(component_id='view2-datetime', component_property='start_date'),
+    State(component_id='view2-datetime', component_property='end_date'),
+)
+def update_view2_images(btn_clicks, cam_loc, start_date, end_date):
+    df_images = dbreid.get_images(
+        human_id=None, start_datetime=start_date, end_datetime=end_date)
+    df_images = df_images[(df_images['human_id'] != '0000') \
+                          & (df_images['human_id'] != '-1')]
 
-            # Draw face detections of each face.
-            for detection in results.detections:
-                #print('Nose tip:')
-                #print(mp_face_detection.get_key_point(detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
-                mp_drawing.draw_detection(annotated_image, detection)
-        except Exception as ex:
-            logging.error(f'detect_face: {ex}')
+    images_col = []
+    for i in range(len(df_images)):
+        encoded_image = base64.b64encode(df_images['img'].iloc[i])
+        images_col.append(
+            dbc.Col(html.Img(
+                src='data:image/png;base64,{}'.format(
+                    encoded_image.decode()),
+                title=df_images['img_id'].iloc[i],
+            )))
 
-        return annotated_image
-
-def detect_face_mesh(img):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_face_mesh = mp.solutions.face_mesh
-
-    with mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5) as face_mesh:
-        annotated_image = img.copy()
-
-        try:
-            # Convert the BGR image to RGB before processing.
-            results = face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-            # Print and draw face mesh landmarks on the image.
-            for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=annotated_image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS)
-        except Exception as ex:
-            logging.error(f'detect_face_mesh: {ex}')
-
-        return annotated_image
-
-def detect_hands(img):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_hands = mp.solutions.hands
-
-    with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5) as hands:
-        annotated_image = img.copy()
-
-        try:
-            # Convert the BGR image to RGB before processing.
-            results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        except Exception as ex:
-            logging.error(f'detect_hands: {ex}')
-
-        return annotated_image
-
-def detect_pose(img):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_pose = mp.solutions.pose
-
-    with mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5) as pose:
-        annotated_image = img.copy()
-
-        try:
-            # Convert the BGR image to RGB before processing.
-            results = pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-            # Draw pose landmarks on the image.
-            # Use mp_pose.UPPER_BODY_POSE_CONNECTIONS for drawing below when
-            # upper_body_only is set to True.
-            mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        except Exception as ex:
-            logging.error(f'detect_pose: {ex}')
-
-        return annotated_image
-
-def detect_holistic(img):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_holistic = mp.solutions.holistic
-
-    with mp_holistic.Holistic(static_image_mode=True) as holistic:
-        annotated_image = img.copy()
-
-        try:
-            # Convert the BGR image to RGB before processing.
-            results = holistic.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-            # Draw pose, left and right hands, and face landmarks on the image.
-
-            mp_drawing.draw_landmarks(annotated_image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-            mp_drawing.draw_landmarks(annotated_image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            mp_drawing.draw_landmarks(annotated_image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            # Use mp_holistic.UPPER_BODY_POSE_CONNECTIONS for drawing below when
-            # upper_body_only is set to True.
-            mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-        except Exception as ex:
-            logging.error(f'detect_holistic: {ex}')
-
-        return annotated_image
+    logs_text = df_images[['img_id', 'human_id',
+                           'inference_datetime']].to_string(justify='center')
+    return images_col, logs_text
