22ffbbd4044d884079e620fc06f5ee8f6382d77d
kenghan77 <kenghan77@users.noreply.github.com>
2021-08-27 00:13:28 +0800

    Adjust code in workstation

--- human_tracker/human_tracker.py
+++ human_tracker/human_tracker.py
@@ -125,16 +125,16 @@ def run_human_tracker(_argv):
             #tf.config.experimental.set_visible_devices(gpus[0:1], 'GPU')
             # Currently, memory growth needs to be the same across GPUs
 
-            for gpu in gpus:
-                tf.config.experimental.set_visible_devices(gpu, 'GPU')
-                tf.config.experimental.set_memory_growth(gpu, True)
-                #tf.config.set_logical_device_configuration(
-                #    gpus[0],
-                #    [tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-                print("[Cam "+str(FLAGS.cam_id)+"]:", len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-                print(gpus)
-                print(logical_gpus)
+            #for gpu in gpus:
+            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+            #tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.set_logical_device_configuration(
+                gpus[0],
+                [tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print("[Cam "+str(FLAGS.cam_id)+"]:", len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            print(gpus)
+            print(logical_gpus)
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
@@ -176,8 +176,8 @@ def run_human_tracker(_argv):
     # load configuration for object detector
     config = ConfigProto()
     #config.gpu_options.per_process_gpu_memory_fraction = 0.2
-    if FLAGS.online:
-        config.gpu_options.visible_device_list = str(FLAGS.gpu)    
+    #if FLAGS.online:
+    #    config.gpu_options.visible_device_list = str(FLAGS.gpu)    
     config.gpu_options.allow_growth = True
     session = InteractiveSession(config=config)
     devices = session.list_devices()
