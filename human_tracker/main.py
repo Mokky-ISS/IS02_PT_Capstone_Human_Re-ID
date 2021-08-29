3d6845e710aac55088803962a8bd4ef67b472fa4
kenghan77 <kenghan77@users.noreply.github.com>
2021-08-30 00:46:18 +0800

    Update 30 Aug

--- human_tracker/main.py
+++ human_tracker/main.py
@@ -37,8 +37,8 @@ flags.DEFINE_integer('input_skip_frame', 8, 'number of frame to be skipped')
 flags.DEFINE_integer('db_skip_frame', 8, 'number of frame to be skipped')
 flags.DEFINE_boolean('saliant_sampling', True, 'select and store unique frame only into database')
 flags.DEFINE_boolean('plot_graph', False, 'plot graph for soft threshold')
-flags.DEFINE_integer('parallel_ps', 2, 'number of human tracker process to run')
-flags.DEFINE_boolean('online', False, 'run online image extraction using rtsp')
+flags.DEFINE_integer('parallel_ps', 8, 'number of human tracker process to run')
+flags.DEFINE_boolean('online', True, 'run online image extraction using rtsp')
 flags.DEFINE_boolean('reid', True, 'set to True to run with REID, set to False if new labelled data are needed to be recorded')
 
 # def db_process(*args):
